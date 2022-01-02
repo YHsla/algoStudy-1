d89d6cf3dba8303b204d87894573527429bec9ff
YYH <zombi907@hanmail.net>
2022-01-02 15:06:57 +0900

    daily commit 22.01.02

--- Problems/1316/ans_1316_YH1.py
+++ Problems/1316/ans_1316_YH1.py
@@ -1,5 +1,4 @@
 #https://www.acmicpc.net/problem/1316
-
 import sys
 
 #쉽게 생각했는데.. 자꾸 꼬이네.. 다시 생각 필요!!
@@ -13,15 +12,15 @@ def solve():
         char_list.append(input().rstrip())
 
     for char in char_list:
-        if len(char) != len(set(char)):
+        if len(char) != len(set(char)): #중복된 문자가 있는 경우
             cnt1 = 1            
             for i in range(len(char)-2):
                 if char[i] != char[i+1] and char[i] in char[i+2:]:
+                    #연속되지 않은 문자열+똑같은 문자 재사용 case
                     cnt1 = 0
             cnt+=cnt1
-        else:
-            cnt+=1
-                   
+        else: #중복된 문자가 없는 경우
+            cnt+=1                   
     print(cnt)
 
 if __name__ == "__main__":
