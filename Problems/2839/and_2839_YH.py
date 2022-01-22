162a11c601f39d3100171aeb5cd7dba71c201550
YYH <zombi907@hanmail.net>
2022-01-22 21:56:05 +0900

    trial

--- Problems/2839/and_2839_YH.py
+++ Problems/2839/and_2839_YH.py
@@ -1,7 +1,7 @@
 #https://www.acmicpc.net/problem/2839
 
 #Bottom-Up 방식
-def solve():
+def solve1():
     N = int(input())
     dp = [-1]*5001 
     # 초기값 설정으로 입력값이 최대 5000 Kg이기 때문에 dp의 크기가 5000이하인 경우, Index error가 발생
@@ -16,6 +16,19 @@ def solve():
         else:
             dp[i] = min(dp[i-3], dp[i-5])+1
     print(dp[N])
+
+#Top-Down 방식 - 어렵군...
+# def solve2():
+#     N = int(input())
+#     global memo
+#     memo ={}
+
+# def dp(n):
+#     if n not in memo:
+#         result = min(dp(n-3), dp(n-5))+1
+#         memo[n] = result
+#     return memo[n]
+
 
 if __name__ == "__main__":
     solve()
