95a63060f6307b4b44c1bc7d4f7bb2b6f35e15b7
YYH <zombi907@hanmail.net>
2022-03-03 21:19:01 +0900

    Daily commit 22.03.03

--- Problems/10773/ans_10773_YH.py
+++ Problems/10773/ans_10773_YH.py
@@ -1,5 +1,55 @@
 # https://www.acmicpc.net/problem/10773
 
-def solve():######
+# def solve():######
     
-    공부중
+#Stack 구현
+# class Stack(list):
+#     def __init__(self):
+#         self.stack=[]
+        
+#     def push(self, data):
+#         self.stack.append(data)
+        
+#     def pop(self):
+#         if self.is_empty():
+#             return -1
+#         else:
+#             self.stack.pop()
+            
+#     def peak(self):
+#         return self.stack[-1]
+    
+#     def is_empty(self):
+#         if len(self.stack) == 0:
+#             return True
+#         return False    
+
+#Singly Linked list를 활용한 Stack 구현
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.head = None
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    def pop(self):
+        if self.is_empty():
+            return -1
+        data = self.head.data
+        self.head = self.head.next
+        return data
+    def is_empty(self):
+        if len(self.head):
+            return True
+        return False
+    def peak(self):
+        if self.is_empty():
+            return -1
+        return self.head.data
+    
+            
+        
