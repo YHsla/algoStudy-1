7c70a592e8b43ef8d9754dbfde9c5585e0349a43
YYH <zombi907@hanmail.net>
2022-04-08 22:46:43 +0900

    daily commit 22.04.08

--- Problems/1158/ans_1158_YH.py
+++ Problems/1158/ans_1158_YH.py
@@ -1,50 +1,47 @@
 #https://www.acmicpc.net/problem/1158
-from re import M
 
-
-MAX_QSIZE = N
-
-class CircularQueue:
-    
-    def __init__(self):
+class CircularQueue():
+    def __init__(self, MAX_QSIZE):
+        self.MAX_QSIZE = MAX_QSIZE+1
         self.front = 0
         self.rear = 0
-        self.queue = [None] * MAX_QSIZE
+        self.queue = [0] * self.MAX_QSIZE
     def isempty(self):
         return (self.front == self.rear)
     def isfull(self):
-        return (self.front == (self.rear+1) % MAX_QSIZE)
+        return (self.front == (self.rear+1) % self.MAX_QSIZE)
     def enqueue(self, data):
         if not self.isfull():
-            self.rear = (self.rear+1)%MAX_QSIZE
             self.queue[self.rear] = data
+            self.rear = (self.rear+1)%self.MAX_QSIZE            
     def dequeue(self):
         if not self.isempty():
-            self.front = (self.front+1)%MAX_QSIZE
-            return self.queue[self.front]
+            temp = self.queue[self.front]
+            self.front = (self.front+1)%self.MAX_QSIZE
+            return temp
 
 def solve():
     global N
     N, K = map(int, input().split())
-    Queue = CircularQueue()
+    Queue = CircularQueue(N)
     res = []
     for i in range(1,N+1):
         Queue.enqueue(i)
-    
+
     while True:
-        i+=1
-        if i%MAX_QSIZE == 3:
-            res.append(Queue.dequeue)
-            i=0
-        else:
-            temp = Queue.dequeue
-            Queue.enqueue(temp)
-        if Queue.isempty:
+        for i in range(K-1):
+            Queue.enqueue(Queue.dequeue())
+        res.append(Queue.dequeue())
+        # i+=1
+        # if (i%N)%K == 0:
+        #     res.append(Queue.dequeue())
+        # else:
+        #     temp = Queue.dequeue()
+            
+        #     Queue.enqueue(temp)
+        if Queue.isempty():
             break
-    #
-
-
-
+    print("<"+str(res)[1:-1]+">")
 
     return
 
