c36dc7f9f3714ebacacb6b031e87ef3b29ef01d1
YYH <zombi907@hanmail.net>
2022-04-13 23:04:59 +0900

    daily commit 22.04.13

--- Problems/2164/ans_2164_YH.py
+++ Problems/2164/ans_2164_YH.py
@@ -1,2 +1,47 @@
 #https://www.acmicpc.net/problem/2164
 #
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+        
+class queue:
+    def __init__(self):
+        self.front = Node(None)
+        self.rear = Node(None)
+        self.front.next = self.rear.prev
+        self.size = 0
+    def qsize(self):
+        return self.size
+    def enqueue(self, data):
+        NewNode = Node(data)
+        NewNode.prev = self.rear
+        self.rear = NewNode
+        NewNode.next = self.rear
+        self.size+=1
+    def dequeue(self):
+        if self.size > 0:
+            temp = self.front.data
+            self.front = self.front.next
+            self.size-=1
+            return temp
+
+
+def solve():
+    N = int(input())
+    numbers = [n for n in range(1,N+1)]
+    Queue = queue()
+    for x in numbers:
+        Queue.enqueue(x)
+    i = 0
+    while (Queue.size > 0):
+        i+=1
+        temp = Queue.dequeue()
+        if i%2 == 0:
+            Queue.enqueue(temp)
+
+    return print(temp)
+
+if __name__ == "__main__":
+    solve()
