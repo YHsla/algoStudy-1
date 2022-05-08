6adb17b10fa53ab8a914af67623b3ca28fdf32a9
YYH <zombi907@hanmail.net>
2022-05-08 15:30:37 +0900

    daily commit 22.05.08

---
+++ Problems/1966/ans_1966_YH.py
@@ -0,0 +1,52 @@
+#https://www.acmicpc.net/problem/1966
+
+class Node:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.next = None
+        self.prev = None
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+    def push(self, data):
+        self.size+=1
+        NewNode = Node(data)
+        if not self.head :
+            self.head = self.tail = NewNode
+            return
+        self.head.prev = NewNode
+        NewNode.next = self.head
+        self.head = NewNode
+    def pop(self):
+        self.size-=1
+        temp = self.tail.data
+        self.tail = self.tail.prev
+        if not self.tail:
+            self.head = None
+        return temp
+
+def solve():
+    T = int(input())
+    for _ in range(T):
+        N, M = map(int, input().split())
+        priority = list(map(int,input.split()))
+        Q = Queue()
+        for i, v in enumerate(priority):
+            Q.push((i,v))
+        priority.sort()
+        cnt = 0
+        while Q:
+            i, x = Q.pop()
+            if x == priority[-1]:
+                priority.pop()
+                cnt+=1
+                if i == M:
+                    print(cnt)
+                    break
+            else:
+                Q.push((i,x))
+
+if __name__ == "__main__":
+    solve()
