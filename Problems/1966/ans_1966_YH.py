858c3221f8b4ad9e79af59bfe58ec8b8a0199363
YYH <zombi907@hanmail.net>
2022-05-09 21:38:11 +0900

    daily commit 22.05.09

--- Problems/1966/ans_1966_YH.py
+++ Problems/1966/ans_1966_YH.py
@@ -1,40 +1,43 @@
 #https://www.acmicpc.net/problem/1966
-
-class Node:
-    def __init__(self,data) -> None:
-        self.data = data
-        self.next = None
-        self.prev = None
-class Queue:
-    def __init__(self):
-        self.size = 0
-        self.head = None
-        self.tail = None
-    def push(self, data):
-        self.size+=1
-        NewNode = Node(data)
-        if not self.head :
-            self.head = self.tail = NewNode
-            return
-        self.head.prev = NewNode
-        NewNode.next = self.head
-        self.head = NewNode
-    def pop(self):
-        self.size-=1
-        temp = self.tail.data
-        self.tail = self.tail.prev
-        if not self.tail:
-            self.head = None
-        return temp
+from collections import deque
+# Queue 구현
+# class Node:
+#     def __init__(self,data) -> None:
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.head = None
+#         self.tail = None
+#     def push(self, data):
+#         self.size+=1
+#         NewNode = Node(data)
+#         if not self.head :
+#             self.head = self.tail = NewNode
+#             return
+#         self.head.prev = NewNode
+#         NewNode.next = self.head
+#         self.head = NewNode
+#     def pop(self):
+#         self.size-=1
+#         temp = self.tail.data
+#         self.tail = self.tail.prev
+#         if not self.tail:
+#             self.head = None
+#         return temp
 
 def solve():
     T = int(input())
     for _ in range(T):
         N, M = map(int, input().split())
-        priority = list(map(int,input.split()))
-        Q = Queue()
+        priority = list(map(int,input().split()))
+        Q = deque() #deque활용
+        # Q = Queue() # 큐 구현
         for i, v in enumerate(priority):
-            Q.push((i,v))
+            # Q.push((i,v))
+            Q.appendleft((i,v)) 
         priority.sort()
         cnt = 0
         while Q:
@@ -46,7 +49,8 @@ def solve():
                     print(cnt)
                     break
             else:
-                Q.push((i,x))
+                # Q.push((i,x))
+                Q.appendleft((i,x))
 
 if __name__ == "__main__":
     solve()
