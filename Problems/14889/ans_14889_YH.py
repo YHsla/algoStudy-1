b892a32dd4ac1e7db04db557310e75a5bdbc58cd
YYH <zombi907@hanmail.net>
2022-06-10 22:53:55 +0900

    daily commit 22.06.10

---
+++ Problems/14889/ans_14889_YH.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/14889
+
+def solve():
+    N = int(input())
+    S = []
+    Capa = []
+    for _ in range(N):
+        S.append(list(map(int, input().split())))
+    
+    return 
+
+#dfs로 풀기
+if __name__ =="__main__":
+    solve()
+
+        
