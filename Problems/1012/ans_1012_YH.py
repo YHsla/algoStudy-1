0491a9cbe613c47ff46f4ed35201ad92537e4c41
YYH <zombi907@hanmail.net>
2022-07-13 20:40:02 +0900

    daily commit 22.07.13

--- Problems/1012/ans_1012_YH.py
+++ Problems/1012/ans_1012_YH.py
@@ -1,46 +1,46 @@
 #https://www.acmicpc.net/problem/1012
 from collections import deque
 
-def bfs(x,y,visited,graph, cnt):
+def bfs(x,y,visited,graph):
     queue = deque()
-    if graph[x][y] == 1:
+    if graph[y][x] == 1:
         queue.append((x,y))
-        visited[x][y] = 1
-        graph[x][y] = 0
+        visited[y][x] = 1
+        graph[y][x] = 0
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<= nx < M and 0<= ny < N:
-                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
+            if 0 <= nx < M and 0 <= ny < N:
+                if graph[ny][nx] == 1 and visited[ny][nx] == 0:
+                    visited[ny][nx] = 1
                     queue.append((nx,ny))
-                    graph[nx][ny] = 0
-    cnt+=1     
-    return cnt
+                    graph[ny][nx] = 0      
+    return
 
 def solve():
     global dx, dy, M, N
     TC = int(input())
-    M, N, K = map(int, input().split())
     dx = [0,0,-1,1]
     dy = [1,-1,0,0]
-    visited = [[0]*M in range(N)]
 
     for _ in range(TC):
-        graph = [[0]*M in range(N)]
+        M, N, K = map(int, input().split())
+        graph = [[0]*M for _ in range(N)]
+        visited = [[0]*M for _ in range(N)]
         cnt = 0
 
         for _ in range(K):    
             a, b = map(int, input().split())
-            graph[a][b] = 1        
+            graph[b][a] = 1        
         
         for i in range(M):
             for j in range(N):
-                if graph[i][j] == 1:
-                    bfs(i, j, visited, graph, cnt)
+                if graph[j][i] == 1:
+                    bfs(i, j, visited, graph)
+                    cnt+=1
         
         print(cnt)
 
