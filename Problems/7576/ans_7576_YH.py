21c220422387af0a62ea5201f4e3f109a57ccd13
YYH <zombi907@hanmail.net>
2022-07-23 23:10:47 +0900

    daily commit 22.07.23

--- Problems/7576/ans_7576_YH.py
+++ Problems/7576/ans_7576_YH.py
@@ -1,6 +1,34 @@
 #https://www.acmicpc.net/problem/7576
+from collections import deque
 
 def solve():
+    global M, N, dx, dy, cnt
+    M, N = map(int, input().split())
+    graph = []
+    visited = [[0]*M for _ in range(N)]
+    dx = [-1,1,0,0]
+    dy = [0,0,1,-1]
+    queue = deque()
+    cnt = 0
+    for _ in range(N):
+        graph.append(list(map(int, input().split(" "))))
+    
+    for i in range(M):
+        for j in range(N):
+            if graph[j][i] == 1:
+                queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+        if graph[y][x] == 1:    
+            for i in range(4):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                if 0 <= nx < M and 0 <= ny < N and visited[ny][nx] == 0:
+                    visited[ny][nx] = 1
+                    graph[ny][nx] = 1
+                    queue.append((nx,ny))
+        cnt +=1
 
     return
 
