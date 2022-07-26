123d94cb93bfc3200b9e92bafee44d4ff2edd624
YYH <zombi907@hanmail.net>
2022-07-26 23:25:11 +0900

    daily commit 22.07.26

--- Problems/7576/ans_7576_YH.py
+++ Problems/7576/ans_7576_YH.py
@@ -8,30 +8,39 @@ def solve():
     dx = [-1,1,0,0]
     dy = [0,0,1,-1]
     queue = deque()
-    cnt = 0
     for _ in range(N):
         graph.append(list(map(int, input().split(" "))))
     
+    isfull = True
     for i in range(N):
         for j in range(M):
             if graph[i][j] == 1:
-                queue.append((i,j))
+                queue.append((i,j,0))
+                visited[i][j] = 1
+            elif graph[i][j] == -1:
+                visited[i][j] = 1
+            else:
+                isfull = False
+    # print(queue)
 
     while queue:
-        x, y = queue.popleft()   
+        temp = len(queue)
+        x, y, cnt = queue.popleft()
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                graph[nx][ny] = 1
-                queue.append((nx,ny))
-        print(queue)
-        if (x+y) != (nx+ny):
-            cnt +=1
-
-    return print(cnt)
-
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 0 and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    graph[nx][ny] = 1
+                    queue.append((nx,ny,cnt+1))
+    
+    if isfull == True:
+        print(0)
+    elif visited != [[1]*M for _ in range(N)]:
+        print(-1)
+    else:
+        print(cnt)
   
 if __name__ == "__main__":
     solve()
